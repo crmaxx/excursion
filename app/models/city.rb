c47ff5bba8954c1c3979d814da6d3160b2ddd612
Maxim Zhukov <crmaxx@gmail.com>
2014-01-26 04:02:49 +0400

    Added StaticPages

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,4 +2,6 @@ class City < ActiveRecord::Base
   has_many :tours
 
   validates :name, presence: true, uniqueness: true
+
+  scope :with_tours, where('tours_count > ?', 0)
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,6 @@
 class Tour < ActiveRecord::Base
   has_and_belongs_to_many :categories
-  belongs_to :city, dependent: :destroy
+  belongs_to :city, counter_cache: true, dependent: :destroy
 
   validates :city_id, presence: true
   validates :name, presence: true, uniqueness: true
