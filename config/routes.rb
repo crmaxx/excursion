5830f5d0b514f5b3d0e0a2af704175e76fe840c4
Maxim Zhukov <crmaxx@gmail.com>
2014-01-28 01:07:31 +0400

    Added public area

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Excursion::Application.routes.draw do
   root 'static_pages#home'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  resources :tours, only: [:index, :show]
+  resources :categories, only: [:index, :show]
+  resources :cities, only: [:index, :show]
 end
