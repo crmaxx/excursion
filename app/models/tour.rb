7867cfc4cc14d6e19922e23c705347f1ad4756c5
Maxim Zhukov <crmaxx@gmail.com>
2014-01-28 00:56:02 +0400

    Changed counter_cache for tours_countv

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,9 +1,19 @@
 class Tour < ActiveRecord::Base
   has_and_belongs_to_many :categories
-  belongs_to :city, counter_cache: true, dependent: :destroy
+  belongs_to :city, dependent: :destroy
 
   validates :city_id, presence: true
   validates :name, presence: true, uniqueness: true
 
   scope :active, -> { where(public: true) }
+  scope :recent, ->(limit=5) { active.order('created_at desc').limit(limit) }
+
+  after_save :update_counter_cache
+  after_destroy :update_counter_cache
+
+  private
+  def update_counter_cache
+    self.city.tours_count = Tour.active.where('city_id = ?', self.city.id).count
+    self.city.save
+  end
 end
