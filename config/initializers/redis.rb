39f5daa97837fea336a9d90355bc05eca318f439
Maxim Zhukov <crmaxx@gmail.com>
2014-01-28 04:21:42 +0400

    Fixed redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,14 @@
+require 'yaml'
+
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env  = ENV['RAILS_ENV'] || 'development'
+
+REDIS_CONFIG = YAML.load(
+  ERB.new(
+    File.read(File.join(rails_root, 'config', 'redis.yml'))
+  ).result
+)[rails_env]
+
 $redis = Redis.new(
   host:     REDIS_CONFIG['address'],
   port:     REDIS_CONFIG['port'],
