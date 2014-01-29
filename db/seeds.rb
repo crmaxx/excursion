19b9984d1430d798ce52f141ec3087a66470db72
Maxim Zhukov <crmaxx@gmail.com>
2014-01-29 10:25:46 +0400

    Added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+$redis.ltrim("#{Tour::REDIS_PREFIX}:#{AdminUser.first.id}", 0, 0)
+
+Tour.destroy_all
+City.destroy_all
+Category.destroy_all
+
+cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }, { name: 'Moscow' }, { name: 'Vladimir' }])
+Category.create!([{ name: 'Historical' }, { name: 'Urban' }, { name: 'Natural' }, { name: 'Museum' }])
+
+Tour.create!(name: 'Awesome tour 1', city: cities.first, public: true, description: 'Awesome tour description 1',
+  categories: Category.where(name: ['Historical', 'Urban']))
+Tour.create!(name: 'Awesome tour 2', city: cities.last, description: 'Awesome tour description 2',
+  categories: Category.where(name: ['Natural', 'Museum']))
+Tour.create!(name: 'Awesome tour 3', city: cities.first, public: true, description: 'Awesome tour description 3',
+  categories: Category.where(name: ['Historical', 'Museum']))
+Tour.create!(name: 'Awesome tour 4', city: cities.first, public: true, description: 'Awesome tour description 4',
+  categories: Category.where(name: ['Historical', 'Museum']))
+Tour.create!(name: 'Awesome tour 5', city: cities.first, public: true, description: 'Awesome tour description 5',
+  categories: Category.where(name: ['Historical', 'Museum']))
+Tour.create!(name: 'Awesome tour 6', city: cities.first, public: true, description: 'Awesome tour description 6',
+  categories: Category.where(name: ['Historical', 'Museum']))
+Tour.create!(name: 'Awesome tour 7', city: cities.first, public: true, description: 'Awesome tour description 7',
+  categories: Category.where(name: ['Historical', 'Museum']))
