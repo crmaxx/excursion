69a2df24365ef69c44f783a0d735e703b45c31c3
Maxim Zhukov <crmaxx@gmail.com>
2014-01-29 09:23:37 +0400

    Added test for public tours

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -14,4 +14,19 @@ describe Tour do
       should validate_uniqueness_of(:name)
     }
   end
+
+  context 'scopes' do
+    describe '.active' do
+      let(:tour) { FactoryGirl.create(:tour) }
+      let(:disabled_tour) { FactoryGirl.create(:disabled_tour) }
+
+      it 'includes tours with public flag' do
+        Tour.active.should include(tour)
+      end
+
+      it 'excludes tours without public flag' do
+        Tour.active.should_not include(disabled_tour)
+      end
+    end
+  end
 end
